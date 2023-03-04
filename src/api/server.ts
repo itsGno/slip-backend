5e869c28851868669ad3700567d1627d9f559381
Geno <nipitpon.gn@gmail.com>
2023-03-05 00:40:50 +0700

    https server

--- src/api/server.ts
+++ src/api/server.ts
@@ -1,5 +1,16 @@
 import app from './app';
+import https from 'https'
+import fs from 'fs'
 
-const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+const options = {
+    key: fs.readFileSync('/etc/ssl/private/nginx.key'),
+    cert: fs.readFileSync('/etc/ssl/certs/nginx.crt')
+  };
+  
+  https.createServer(options, app).listen(3030, () => {
+    console.log('Server running on port 3000');
+  });
+const PORT = process.env.PORT || 3030;
+
+// app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
